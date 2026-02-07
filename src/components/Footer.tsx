1890ede84849e87d57e4c8dc0c7dd49f211931f3
jsanjosel01 <jsanjosel01@iesalbarregas.es>
2026-02-07 13:29:22 +0100

    ✅New Create: Creación del componente footer.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+// src/components/Footer.tsx
+export default function Footer() {
+  return (
+    <footer className="w-full py-8 mt-auto bg-[#121212] text-center">
+      <p className="text-gray-500 text-xs uppercase tracking-widest">
+        © 2026 EntrenaTU - “Creado por: Javier Villar, Adran Herrera, Julia San José & Alba Barroso”
+                                     
+      </p>
+    </footer>
+  );
+}
