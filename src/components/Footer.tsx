bac4f82984cda488083d9a149311be1556a4d0a4
jsanjosel01 <jsanjosel01@iesalbarregas.es>
2026-02-07 14:14:38 +0100

    🔄️Update: Modificar el footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,15 @@
 // src/components/Footer.tsx
+
 export default function Footer() {
+
   return (
-    <footer className="w-full py-8 mt-auto bg-[#121212] text-center">
-      <p className="text-gray-500 text-xs uppercase tracking-widest">
-        © 2026 EntrenaTU - “Creado por: Javier Villar, Adran Herrera, Julia San José & Alba Barroso”
-                                     
-      </p>
+
+    <footer className="w-full py-6 bg-[#4B4B4B] text-[#CACACA] text-center mt-auto border-t-2 border-[#FF8904]">
+      <div className="container mx-auto px-4">
+        <p className="font-open-sans text-[13px] lowercase tracking-wide leading-relaxed">
+          © 2026 Creado por: Javier Villar, Adrian Herrera, Julia San José & Alba Barroso
+        </p>
+      </div>
     </footer>
   );
 }
