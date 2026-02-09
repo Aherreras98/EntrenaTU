979cc4832f7ccac974da87f28d3702bc0f5553e4
JaviVillar10 <javi.villar.jimenez@gmail.com>
2026-02-09 13:55:44 +0100

    ✅New: Creacion Tabla historial

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,9 +1,17 @@
-
+import HistoryTable from "../components/common/HistoryTable";
 export default function History() {
 
     return (
         <div>
             <h1>History</h1>
+
+            <p className="text-zinc-400 mt-2 font-open-sans">
+            Consulta tus entrenamientos pasados y analiza tu rendimiento.
+          </p>
+
+         <section className="animate-in fade-in slide-in-from-bottom-4 duration-700">
+          <HistoryTable />
+        </section>
         </div>
     );
 }
