7aa79714d63ef71c5443f07e4eb321f672df4388
JaviVillar10 <javi.villar.jimenez@gmail.com>
2026-02-20 02:54:58 +0100

    🔥Fix: Correccion en el registro, ahora si que crea los usuarios en la base de datos

--- src/database/supabase/SupabaseUserRepository.ts
+++ src/database/supabase/SupabaseUserRepository.ts
@@ -1,31 +1,21 @@
 import { UserRepository } from "../repositories/UserRepository";
 import { SignUpData } from "../../interfaces/SignUpData";
-import { supabase } from "./Client"; // Asegúrate de tener configurado el cliente de Supabase aquí
+import { supabase } from "./client"; 
 
 export class SupabaseUserRepository implements UserRepository {
-
     async register(user: SignUpData): Promise<string | null> {
-        try {
-            const { data, error } = await supabase.auth.signUp({
-                email: user.email,
-                password: user.password,
-                options: {
-                    data: {
-                        username: user.username,
-                        age: user.age,
-                    },
-                },
-            });
-
-            if (error) {
-                throw new Error(error.message);
+        const { data, error } = await supabase.auth.signUp({
+            email: user.email,
+            password: user.password,
+            options: {
+                data: {
+                    username: user.username,
+                    age: user.age
+                }
             }
+        });
 
-            return data.user?.id || null;
-
-        } catch (error) {
-            console.error("Error en el registro:", error);
-            throw error;
-        }
+        if (error) throw new Error(error.message);
+        return data.user?.id || null;
     }
 }
