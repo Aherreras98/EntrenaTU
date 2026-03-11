05a35583e4a076412c5f9fa51a668e06ec0a830f
JaviVillar10 <javi.villar.jimenez@gmail.com>
2026-03-11 16:27:46 +0100

    ✅New: Creacion del avatar

---
+++ src/database/repositories/StorageRepository.ts
@@ -0,0 +1,4 @@
+export interface StorageRepository {
+    uploadFile(bucketName: string, filePath: string, file: File): Promise<{ data?: { publicUrl: string }; error?: any; }>;
+    getPublicUrl(bucketName: string, filePath: string): Promise<{ data: { publicUrl: string } }>;
+}
