e276d3c3c4515e39b6f8ef1935200e790b8c97e7
Adrián  HerreraC:/Users/elavi/.virtualenvs/Ejercicio_6-JBbSqsPY/Scripts/python.exe <aherrerasosa98@gmail.com>
2026-03-13 00:13:42 +0100

    ✅New: SpeedInsights Vercel

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import '../i18n';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
 
